05e548ce3e830b3d45c6e340678a401c5c8e896c
Kato Hiroki <k.hiro1818@gmail.com>
2020-03-28 06:18:47 +0000

    Enable to use multiple components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
+import Rect from './Rect';
 import './App.css';
 
-class App extends React.Component {
-
-  constructor(props) {
-    super();
-    this.title = props.title;
-    this.message = props.message;
-  }
-
+class App extends Component {
   render() {
-    return (
-      <div>
-        <h1>{this.title}</h1>
-        <p>{this.message}</p>
-      </div>
-    );
+    return <div>
+      <h1>React</h1>
+      <Rect x="50" y="50" w="150" h="150" c="#6ff9" r="50"/>
+      <Rect x="150" y="100" w="150" h="150" c="#f6f9" r="75" />
+      <Rect x="100" y="150" w="150" h="150" c="#6669" r="25" />
+    </div>;
   }
 }
 
