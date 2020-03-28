78b3a48746ff1a768ba84ee704d6a1603b1276b2
Kato Hiroki <k.hiro1818@gmail.com>
2020-03-28 09:26:14 +0000

    Enable to bind event

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,26 +19,38 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      msg: 'Hello Component.',
+      counter: 0,
+      msg: 'count start!',
     };
 
     // ステートを更新している
-    let timer = setInterval(() => {
-      this.setState((state) => ({
-        msg: state.msg + "!"
-      }));
-    }, 10000)
+    // let timer = setInterval(() => {
+    //   this.setState((state) => ({
+    //     msg: state.msg + "!"
+    //   }));
+    // }, 10000)
+
+    this.doAction = this.doAction.bind(this);
+  }
+
+  // e: イベントの情報をまとめたオブジェクトが入っている
+  doAction(e) {
+    this.setState((state) => ({
+      counter: state.counter + 1,
+      msg: 'count: ' + state.counter
+    }));
   }
 
   render() {
     return <div>
       <h1>React</h1>
-      <Rect x="50" y="50" w="150" h="150" c="#6ff9" r="50"/>
+      {/* <Rect x="50" y="50" w="150" h="150" c="#6ff9" r="50"/>
       <Rect x="150" y="100" w="150" h="150" c="#f6f9" r="75" />
-      <Rect x="100" y="150" w="150" h="150" c="#6669" r="25" />
+      <Rect x="100" y="150" w="150" h="150" c="#6669" r="25" /> */}
 
       <p style={this.msgStyle}>{this.state.msg}</p>
-      <p style={this.msgStyle}>{this.props.msg}</p>
+      <button style={this.btnStyle} onClick={this.doAction}>click</button>
+      {/* <p style={this.msgStyle}>{this.props.msg}</p> */}
     </div>;
   }
 }
